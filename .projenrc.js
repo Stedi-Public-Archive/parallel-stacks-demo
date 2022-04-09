c327c738882475da6c83fe4c42da39ba4356869f
Ken Winner <ken@stedi.com>
2022-04-09 10:22:16 -0400

    chore: disable upgrade workflow

--- .projenrc.js
+++ .projenrc.js
@@ -1,36 +1,36 @@
-const { awscdk, javascript } = require('projen');
-const { GraphDeployWorkflow } = require('./.projen/workflows/graphDeploy');
+const { awscdk, javascript } = require("projen");
+const { GraphDeployWorkflow } = require("./.projen/workflows/graphDeploy");
 
 const project = new awscdk.AwsCdkTypeScriptApp({
-  name: 'parallel-stacks-demo',
-  defaultReleaseBranch: 'main',
+  name: "parallel-stacks-demo",
+  defaultReleaseBranch: "main",
   packageManager: javascript.NodePackageManager.NPM,
 
-  cdkVersion: '1.134.0',
+  cdkVersion: "1.134.0",
   cdkVersionPinning: true,
   cdkDependencies: [
-    '@aws-cdk/aws-cloudwatch',
-    '@aws-cdk/aws-dynamodb',
-    '@aws-cdk/aws-events',
-    '@aws-cdk/aws-events-targets',
-    '@aws-cdk/aws-iam',
-    '@aws-cdk/aws-lambda',
-    '@aws-cdk/aws-lambda-nodejs',
-    '@aws-cdk/aws-secretsmanager',
-    '@aws-cdk/aws-ssm',
+    "@aws-cdk/aws-cloudwatch",
+    "@aws-cdk/aws-dynamodb",
+    "@aws-cdk/aws-events",
+    "@aws-cdk/aws-events-targets",
+    "@aws-cdk/aws-iam",
+    "@aws-cdk/aws-lambda",
+    "@aws-cdk/aws-lambda-nodejs",
+    "@aws-cdk/aws-secretsmanager",
+    "@aws-cdk/aws-ssm",
   ],
 
-  devDeps: [
-    'esbuild',
-  ],
+  devDeps: ["esbuild"],
+
+  depsUpgrade: false,
 });
 
-project.addTask('generate:stack-graphs', {
-  exec: 'node bin/generateStackGraphs.js',
+project.addTask("generate:stack-graphs", {
+  exec: "node bin/generateStackGraphs.js",
 });
 
 new GraphDeployWorkflow(project, {
-  name: 'Deploy',
+  name: "Deploy",
 });
 
 project.synth();
